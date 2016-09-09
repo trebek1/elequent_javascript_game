461274da2e6977c6eeb996d3a44d5a979dd31cf7
trebek1 <trebek08@gmail.com>
2016-09-08 19:42:52 -0700

    platform game constructors

---
+++ platform/platform.js
@@ -0,0 +1,151 @@
+var simpleLevelPlan = [
+"                      ",
+"					   ",
+"  X              = X  ",
+"  X		  o o	   ",
+"  X @       XXXXX     ",
+"  XXXXX			   ",
+"      X!!!!!!!!!!!!X  ",
+"	   XXXXXXXXXXXXXX  ",
+"                      "
+];
+
+
+// @ where player starts 
+// = block of lava that moves bakc and forth orizontally
+// | moving blob of lava 
+// V for dripping lava 
+// X is wall 
+
+// build a basic level 
+
+function Level(plan){
+
+	this.width = plan[0].length; 
+	this.height = plan.length; 
+	this.grid = []; 
+	this.actors = []; 
+
+	for(var y = 0; y<this.height; y++){
+		var line = plan[y],
+			gridLine = []; 
+		for(var x = 0; x<this.width; x++){
+			var ch = line[x], 
+				fieldType = null; 
+			var Actor = actorChars[ch]; 
+			if(Actor){
+				this.actors.push(new Actor(new Vector(x,y),ch)); 
+			}else if( ch === 'X'){
+				fieldType = 'wall';
+			}else if(ch === '!'){
+				fieldType = 'lava';
+			}
+			gridLine.push(fieldType); 
+		}
+		this.grid.push(gridLine); 
+	}
+	this.player = this.actors.filter(function(actor){
+		return actor.type === 'player';
+	})[0];
+	this.status = this.finishDelay = null; 
+}
+// check to see if the level is finished 
+
+Level.prototype.isFinished = function(){
+	return this.status !== null && this.finishDelay < 0; 
+}; 
+
+// coding Actors (dynamic players)
+
+function Vector(x,y){
+	this.x = x; 
+	this.y = y; 
+}
+
+Vector.prototype.plus = function(other){
+	return new Vector(this.x + other.x, this.y + other.y);
+}
+
+Vector.prototype.times = function(factor){
+	return new Vector(this.x * factor, this.y * factor);
+}
+
+var actorChars = {
+	"@" : Player, 
+	"o" : Coin, 
+	"=" : Lava, 
+	"|" : Lava,
+	"v" : Lava
+}
+
+// Player constructor 
+function Player(pos){
+	this.pos = pos.plus(new Vector(0,-0.5));
+	this.size = new Vector(0.8, 1.5);
+	this.speed = new Vector(0,0);
+}
+
+Player.prototype.type = "player"; 
+
+// Lava constructor 
+
+function Lava(pos, ch){
+	this.pos = pos; 
+	this.size = new Vector(1,1);
+	if(ch === "="){
+		this.speed = new Vector(2,0);
+	}else if(ch === "|"){
+		this.speed = new Vector(0,2);
+	}else if(ch === "v"){
+		this.speed = new Vector(0,3);
+		this.repeatPos = pos; 
+	}
+}
+Lava.prototype.type = "lava"; 
+
+// coin constructor 
+
+function Coin(pos){
+	this.basePos = this.pos = pos.plus(new Vector(0.2,0.1));
+	this.size = new Vector(0.6,0.6);
+	this.wobble = Math.random()*Math.PI*2; 
+}
+Coin.prototype.type = 'coin'; 
+
+var simpleLevel = new Level(simpleLevelPlan); 
+console.log(simpleLevel.width, ' by ', simpleLevel.height); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
